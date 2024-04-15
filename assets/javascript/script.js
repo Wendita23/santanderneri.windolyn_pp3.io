db2515906bb33f09cd3a649e9808e46eda1272b5
wendita23 <wfsantander@gmail.com>
2024-04-15 19:06:22 +0800

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -11,24 +11,27 @@ window.onscroll = function() {
     }
 }
 
-const API_KEY = 'AIzaSyA8oXBCCRMY_REPjCuf3LlVOfMeSqF4ZeQ';
+const API_KEY = 'YOUR_API_KEY';
 const VIDEO_IDS = ['wvDB3ibuUg4','AlU3TX7DI4&t', 'FNb8YI1rXJw&t','tRSGwnngqD4&t', 'GyJi0_T6i5U', 'DDQxjOVz3wI&t'];
 
-const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${VIDEO_ID}&key=${API_KEY}&part=snippet`;
-
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Handle the data returned from the API
-    console.log(data);
-    // Access video information from data.snippet
-  })
-  .catch(error => {
-    // Handle errors
-    console.error('There was a problem with the fetch operation:', error);
-  });
+// Loop through each video ID and fetch data for each video
+VIDEO_IDS.forEach(videoId => {
+    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${API_KEY}&part=snippet`;
+    
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the data returned from the API
+            console.log(data);
+            // Access video information from data.snippet
+        })
+        .catch(error => {
+            // Handle errors
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
