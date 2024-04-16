810976c51691b656d71e717c1184c74460bbb297
wendita23 <wfsantander@gmail.com>
2024-04-16 19:10:29 +0800

    update pp3

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,3 @@
-// add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
@@ -28,43 +27,31 @@ VIDEO_IDS.forEach(videoId => {
         .then(data => {
             // Handle the data returned from the API
             console.log(data);
-            // Access video information from data.snippet
+             // Access video information from data.snippet
+             const videoDataDiv = document.getElementById('videoData');
+             const videoTitle = document.createElement('h2');
+             videoTitle.textContent = data.items[0].snippet.title;
+             videoDataDiv.appendChild(videoTitle);
+         })
         })
         .catch(error => {
             // Handle errors
             console.error('There was a problem with the fetch operation:', error);
         });
-});
 
-$(document).ready(function(){
-    // Activate Carousel
-    $("#album").carousel();
-      
-    // Enable Carousel Indicators
-    $(".active item").click(function(){
-      $("#album").carousel(0);
-    });
-    $(".item 2").click(function(){
-      $("#album").carousel(1);
-    });
-    $(".item 3").click(function(){
-      $("#album").carousel(2);
-    });
-    $(".item 4").click(function(){
-      $("#album").carousel(3);
-    });
-    $(".item 5").click(function(){
-        $("#album").carousel(4);
-    });
-    $(".item 6").click(function(){
-        $("#album").carousel(5);
-    });
-      
-    // Enable Carousel Controls
-    $(".left").click(function(){
-      $("#album").carousel("prev");
-    });
-    $(".right").click(function(){
-      $("#album").carousel("next");
-    });
-  });
+
+<script>
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+  key: "AIzaSyA0QUcVcz_Akev7ODchAkv8sYrdNULCLNw",
+  v: "weekly",
+  // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+  // Add other bootstrap parameters as needed, using camel case.
+});
+</script>
+function myMap() {
+    var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
